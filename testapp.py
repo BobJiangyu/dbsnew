e2140f8f2d20625172d2b5fb14207a67705690a4
BobJiangyu <97886701+BobJiangyu@users.noreply.github.com>
2022-07-06 13:38:35 +0800

    Add files via upload

---
+++ testapp.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 17:32:25 2022
+
+@author: 23637
+"""
+
+from flask import Flask, render_template,request
+import joblib
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model = joblib.load("regression")
+        r = model.predict([[rates]])
+        return render_template("testpage.html",result=r)
+    else:
+        return render_template("testpage.html",result="WAITING")
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=int("80"))
+
+
+
